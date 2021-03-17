0a25bb878b05d2afa5e7d94738d21c07f03efc13
sasa-bonta <bonta2000@mail.ru>
2021-03-17 11:57:10 +0200

    total sec

--- Lab3/BeerMile.py
+++ Lab3/BeerMile.py
@@ -10,6 +10,7 @@ class BeerMile:
         self.login_url = 'https://www.beermile.com/?action=login'
         self.top_1000 = 'https://www.beermile.com/records/ref_wr'
         self.content = ''
+        self.records = []
 
     def login(self):
         values = {'username': self.username,
@@ -29,8 +30,6 @@ class BeerMile:
         # print(table_html)
 
         # Parse table
-        records = []
-
         nr = 0
         for table_row in table_html.findAll("tr"):
             # Find Name
@@ -38,7 +37,8 @@ class BeerMile:
             if name:
                 name = name.group(1)
             # Find Time
-            time = re.search("\>([\s]*[0-9]+[\:]+[0-9]+[\.]*[0-9]*[\s]*)\<", str(table_row.findAll("td", class_="records_time")))
+            time = re.search("\>([\s]*[0-9]+[\:]+[0-9]+[\.]*[0-9]*[\s]*)\<",
+                             str(table_row.findAll("td", class_="records_time")))
             if time:
                 time = time.group(1)
                 time = re.sub(r"[\n\t\s]*", "", time)
@@ -49,17 +49,38 @@ class BeerMile:
                 year = re.sub(r"[\n\t\s]*", "", year)
             # Find Beer
             # beer = table_row.findAll("td", nowrap="")
-            beer = re.search("beertype_[a-zA-Z\"\+\s]+\>([a-zA-Z\s]+)\<\/a\>\<\/td\>", str(table_row.findAll("td", nowrap="")))
+            beer = re.search("beertype_[a-zA-Z\"\+\s]+\>([a-zA-Z\s]+)\<\/a\>\<\/td\>",
+                             str(table_row.findAll("td", nowrap="")))
             if beer:
                 beer = beer.group(1)
-            records.append(Record(nr, name, time, year, beer))
+            self.records.append(Record(nr, name, time, year, beer))
             nr += 1
 
-        records[0] = Record("Nr.", "Name", "Time", "Year", "Beer")
+        self.records[0] = Record("Nr.", "Name", "Time", "Year", "Beer")
 
-        for rec in records:
-            print(rec.record_position,
-                  " | ", rec.records_name,
-                  " | ", rec.records_time,
-                  " | ", rec.records_year,
-                  " | ", rec.records_beer, "\n")
+
+        # for rec in self.records:
+        #     print(rec.record_position,
+        #           # " | ", rec.records_name,
+        #           # " | ", rec.records_time,
+        #           # " | ", rec.records_year,
+        #           # " | ", rec.records_beer, "\n")
+
+        for rec in self.records:
+            if rec.records_time != "Time":
+                topTimeDiff(rec.records_time)
+
+def topTimeDiff(time):
+    split_time = time.split(":")
+    min = 0.0
+    sec = 0.0
+    n = 0
+    for t in split_time:
+        if n == 0:
+            min = t
+        if n == 1:
+            sec = t
+        n += 1
+    sec_tot = float(sec) + (float(min) * 60)
+    print(sec_tot)
+
--- Lab3/Main.py
+++ Lab3/Main.py
@@ -1,6 +1,5 @@
 from BeerMile import BeerMile
 
-
 # Sasha sasa123
 # Main.py bonta2000@mail.ru 9yG^VVSu+T3=jXw
 
@@ -10,3 +9,10 @@ if __name__ == "__main__":
     beer_mile.login()
     beer_mile.getPage()
     beer_mile.getTop1000()
+
+    # for rec in beer_mile.records:
+    #     print(rec.record_position,
+    #           " | ", rec.records_name,
+    #           " | ", rec.records_time,
+    #           " | ", rec.records_year,
+    #           " | ", rec.records_beer, "\n")
