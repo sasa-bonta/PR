d7c8f6fcdbeb431a6ac3b27dbd338c9ba79557d8
sasa-bonta <bonta2000@mail.ru>
2021-03-17 20:25:42 +0200

    4 Threads, semafor

--- Lab3/BeerMile.py
+++ Lab3/BeerMile.py
@@ -2,15 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 from Record import Record
 import re
+import threading
+from threading import Thread
 
-class BeerMile:
+sem = threading.Semaphore(2)
+
+
+class BeerMile(Thread):
     def __init__(self, email, password):
+        Thread.__init__(self)
         self.username = email
         self.password = password
         self.login_url = 'https://www.beermile.com/?action=login'
         self.top_1000 = 'https://www.beermile.com/records/ref_wr'
         self.content = ''
         self.records = []
+        self.time_best_alco_runner = ''
+        self.sec_best_alco_runner = 0.0
+        self.rec1 = []  # 2 - 250
+        self.rec2 = []  # 251 - 500
+        self.rec3 = []  # 501 - 750
+        self.rec4 = []  # 751 - 1000
 
     def login(self):
         values = {'username': self.username,
@@ -57,30 +69,81 @@ class BeerMile:
             nr += 1
 
         self.records[0] = Record("Nr.", "Name", "Time", "Year", "Beer")
+        rec_best = self.records[1]
+        self.time_best_alco_runner = rec_best.records_time
+        # print(self.time_best_alco_runner)
+        self.timeToSec()
+        rec_best = self.records[1]
+        self.sec_best_alco_runner = rec_best.records_time
+        # print(self.sec_best_alco_runner)
+        self.divideList()
+        self.startThreads()
 
-
-        # for rec in self.records:
-        #     print(rec.record_position,
-        #           # " | ", rec.records_name,
-        #           # " | ", rec.records_time,
-        #           # " | ", rec.records_year,
-        #           # " | ", rec.records_beer, "\n")
-
+    def timeToSec(self):
         for rec in self.records:
             if rec.records_time != "Time":
-                topTimeDiff(rec.records_time)
+                split_time = rec.records_time.split(":")
+                min = 0.0
+                sec = 0.0
+                n = 0
+                for t in split_time:
+                    if n == 0:
+                        min = t
+                    if n == 1:
+                        sec = t
+                    n += 1
+                sec_tot = float(sec) + (float(min) * 60)
+                rec.records_time = sec_tot
 
-def topTimeDiff(time):
-    split_time = time.split(":")
-    min = 0.0
-    sec = 0.0
-    n = 0
-    for t in split_time:
-        if n == 0:
-            min = t
-        if n == 1:
-            sec = t
-        n += 1
-    sec_tot = float(sec) + (float(min) * 60)
-    print(sec_tot)
+    def divideList(self):
+        for i in range(2, 251):
+            self.rec1.append(self.records[i])
+        for i in range(251, 501):
+            self.rec2.append(self.records[i])
+        for i in range(501, 751):
+            self.rec3.append(self.records[i])
+        for i in range(751, 1001):
+            self.rec4.append(self.records[i])
 
+    def getTimeDiff(self, rows):
+        sem.acquire()
+        for rec in rows:
+            diff_sec = rec.records_time - self.sec_best_alco_runner
+            min = int(diff_sec) // 60
+            sec = round((float(diff_sec) % 60), 2)
+            rec.records_time = "+" + str(min) + ":" + str(sec)
+        print(threading.current_thread())
+        sem.release()
+
+    def startThreads(self):
+        threads_list = []
+        t1 = Thread(target=self.getTimeDiff, args=([self.rec1]))
+        t2 = Thread(target=self.getTimeDiff, args=([self.rec2]))
+        t3 = Thread(target=self.getTimeDiff, args=([self.rec3]))
+        t4 = Thread(target=self.getTimeDiff, args=([self.rec4]))
+
+        threads_list.append(t1)
+        threads_list.append(t2)
+        threads_list.append(t3)
+        threads_list.append(t4)
+
+        t1.start()
+        t2.start()
+        t3.start()
+        t4.start()
+
+        for thread in threads_list:
+            thread.join()
+
+        rec = self.records
+        best_alco_runner = rec[1]
+        best_alco_runner.records_time = self.time_best_alco_runner
+        self.displayTable()
+
+    def displayTable(self):
+        for rec in self.records:
+            print(format(str(rec.record_position), ' ^3s'),
+                  " | ", format(str(rec.records_name), ' ^25s'),
+                  " | ", format(str(rec.records_time), ' ^9s'),
+                  " | ", format(str(rec.records_year), ' ^5s'),
+                  " | ", format(str(rec.records_beer), ' ^20s'), "\n")
